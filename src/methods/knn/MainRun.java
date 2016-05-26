b843ed5b4b9de2c0c7f68a96835f7a948b19c501
Tim <tsonderen108@gmail.com>
2016-05-26 13:09:31 +0200

    knn working

--- src/methods/knn/MainRun.java
+++ src/methods/knn/MainRun.java
@@ -5,6 +5,8 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by Tim on 25/05/2016.
@@ -29,27 +31,27 @@ public class MainRun {
 
         BufferedReader BR = new BufferedReader(new FileReader(pathtTrain));
         String line;
-        while((line = BR.readLine()) != null){
+        while ((line = BR.readLine()) != null) {
             //read, split line
             String[] words = line.split(",");
 
             // check if there is the right number of attributes + 1 class
-            if (words.length != numFeatures+1) {
+            if (words.length != numFeatures + 1) {
                 continue;
             }
 
             // determine class
-            String s = (String)words[numFeatures];
+            String s = (String) words[numFeatures];
             String c = "";
-            if(s.equals(walking)) c = walking;
-            else if(s.equals(running)) c = running;
-            else if(s.equals(cycling)) c = cycling;
-            else if(s.equals(car)) c = car;
+            if (s.equals(walking)) c = walking;
+            else if (s.equals(running)) c = running;
+            else if (s.equals(cycling)) c = cycling;
+            else if (s.equals(car)) c = car;
             else System.out.println("### shit fucked up");
 
             // parse the attributes to doubles
-            for(int i = 0; i < numFeatures; i++) {
-                Double d =  Double.parseDouble(words[i]);
+            for (int i = 0; i < numFeatures; i++) {
+                Double d = Double.parseDouble(words[i]);
                 Feature f = new Feature(c, "" + i, d);
                 data[i].add(f);
             }
@@ -79,6 +81,34 @@ public class MainRun {
         space.k = 1;
 
         // now for each test make an instance and test it.
+        int total = 0;
+        int correct = 0;
+        System.out.println("Expected\t\t\t\tResult");
+        BR = new BufferedReader(new FileReader(pathTest));
+        while ((line = BR.readLine()) != null) {
+            //read, split line
+            String[] words = line.split(",");
 
+            // check if there is the right number of attributes + 1 class
+            if (words.length != numFeatures + 1) {
+                continue;
+            }
+
+            Map<String, Double> attributes = new HashMap<String, Double>();
+            for (int i = 0; i < numFeatures; i++) {
+                attributes.put("" + i, Double.parseDouble(words[i]));
+            }
+
+            Instance<String> instance = new Instance(attributes);
+            space.compute(instance, false);
+
+            String result = instance.category;
+            String actual = words[numFeatures];
+            System.out.println(actual + "\t\t\t\t" + result);
+
+            total++;
+            if(result.equals(result)) correct++;
+        }
+        System.out.println(100 * correct / total + "%");
     }
 }
